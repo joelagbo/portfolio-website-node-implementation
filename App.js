ea49e5d25461f4d83175a224c2eaf21653594f7d
joelagbo <agbojoeli@gmail.com>
2020-09-06 20:51:07 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const portfolioServer = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "portfolio",
+    req.url == "/" ? "index.html" : req.url
+  );
+
+  let fileExtension = path.extname(filePath);
+
+  let contentType = "text/html";
+  switch (fileExtension) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/png";
+      break;
+  }
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.writeHead(200, { "content-type": contentType });
+      res.end(data, "utf8");
+    }
+  });
+});
+
+const runningPort = process.env.PORT || 5000;
+
+portfolioServer.listen(runningPort, () =>
+  console.log(`server running on port ${runningPort}`)
+);
